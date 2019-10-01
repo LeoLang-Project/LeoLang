441b1914e8fafc26e8d9b4e21cd2abdf25c5284a
Furesoft <filmee24@gmail.com>
2019-10-01 19:40:59 +0200

    added tests

--- LeoLangTests/ParserTests.cs
+++ LeoLangTests/ParserTests.cs
@@ -1,19 +1,32 @@
 using NUnit.Framework;
 using LeoLang.Core;
+using LeoLang.Core.AST;
 
 namespace Tests
 {
     public class ParserTests
     {
+        [Test]
+        public void IdentiferParsse_Should_Match()
+        {
+            var result = p.Parse("_abc123");
+
+            Assert.IsTrue(((IdentifierNode)result).Name == "_abc123");
+        }
+
         [SetUp]
         public void Setup()
         {
+            p = new LeoParser();
         }
 
         [Test]
         public void Test1()
         {
+            var result = p.Parse("let abc = true");
             Assert.Pass();
         }
+
+        private LeoParser p;
     }
 }
