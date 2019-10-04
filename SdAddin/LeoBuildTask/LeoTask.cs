72f2e8591b7187c2d543c3e89316dfcb737e38b1
Furesoft <filmee24@gmail.com>
2019-10-04 19:51:07 +0200

    addin cleanup

--- SdAddin/LeoBuildTask/LeoTask.cs
+++ SdAddin/LeoBuildTask/LeoTask.cs
@@ -11,41 +11,11 @@ namespace ICSharpCode.Build.Tasks
 {
     public sealed class Leo : ToolTask
     {
-        public ITaskItem OutputAssembly
-        {
-            get
-            {
-                return outputAssembly;
-            }
-            set
-            {
-                outputAssembly = value;
-            }
-        }
+        public ITaskItem OutputAssembly { get; set; }
 
-        public ITaskItem[] Sources
-        {
-            get
-            {
-                return sources;
-            }
-            set
-            {
-                sources = value;
-            }
-        }
+        public ITaskItem[] Sources { get; set; }
 
-        public string TargetType
-        {
-            get
-            {
-                return targetType;
-            }
-            set
-            {
-                targetType = value;
-            }
-        }
+        public string TargetType { get; set; }
 
         protected override string ToolName
         {
@@ -75,8 +45,8 @@ namespace ICSharpCode.Build.Tasks
                 }
             }
 
-            opt.Output = outputAssembly.ItemSpec;
-            opt.Input = sources.First().ItemSpec;
+            opt.Output = OutputAssembly.ItemSpec;
+            opt.Input = Sources.First().ItemSpec;
 
             var cmdline = Parser.Default.FormatCommandLine<Options>(opt);
             File.WriteAllText(@"C:\Users\filmee24\Documents\cmd.txt", cmdline);
@@ -86,27 +56,6 @@ namespace ICSharpCode.Build.Tasks
         protected override string GenerateFullPathToTool()
         {
             return this.GetWorkingDirectory();
-            string path = ToolLocationHelper.GetPathToDotNetFrameworkFile(ToolName, TargetDotNetFrameworkVersion.VersionLatest);
-            if (path == null)
-            {
-                base.Log.LogErrorWithCodeFromResources("General.FrameworksFileNotFound", ToolName, ToolLocationHelper.GetDotNetFrameworkVersionFolderPrefix(TargetDotNetFrameworkVersion.VersionLatest));
-            }
-            return path;
-        }
-
-        private string debugType;
-        private string emitDebugInformation;
-        private int fileAlignment;
-        private string keyContainer, keyFile;
-        private bool optimize;
-        private ITaskItem outputAssembly;
-        private ITaskItem[] resources;
-        private ITaskItem[] sources;
-        private string targetType;
-
-        private void AppendIntegerSwitch(CommandLineBuilder commandLine, string @switch, int value)
-        {
-            commandLine.AppendSwitchUnquotedIfNotNull(@switch, value.ToString(NumberFormatInfo.InvariantInfo));
         }
     }
 }
