bf2762c11d34d89ff347967171019abd34956503
Furesoft <filmee24@gmail.com>
2019-10-05 09:39:42 +0200

    added ctor to methoddefinitionnode

--- LeoLang.Core/AST/MethodDefinitionNode.cs
+++ LeoLang.Core/AST/MethodDefinitionNode.cs
@@ -3,8 +3,17 @@
     public class MethodDefinitionNode : SyntaxNode
     {
         public SyntaxNode Body { get; set; }
+
         public string Name { get; set; }
+
         public string ReturnType { get; set; }
+
+        public MethodDefinitionNode(string name, string returnType, SyntaxNode body)
+        {
+            Name = name;
+            ReturnType = returnType;
+            Body = body;
+        }
 
         public override void Accept(Visitor visitor)
         {
