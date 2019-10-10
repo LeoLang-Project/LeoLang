aed92505e0d18a0385933da43feb1532a512870a
Furesoft <filmee24@gmail.com>
2019-10-10 21:16:45 +0200

    added dump visitor core

---
+++ LeoLangCompiler/DumpVisitor.cs
@@ -0,0 +1,12 @@
+﻿using LeoLang.Core;
+
+namespace LeoLangCompiler
+{
+    public class DumpVisitor : Visitor
+    {
+        public override void Visit(SyntaxNode n)
+        {
+            YamlDotNet.Serialization.Serializer.
+        }
+    }
+}
