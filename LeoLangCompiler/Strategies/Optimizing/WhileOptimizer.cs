7ac112f10a3b7326715405354b518686a918214b
Furesoft <filmee24@gmail.com>
2019-10-14 16:43:51 +0200

    added simple while optimizer

---
+++ LeoLangCompiler/Strategies/Optimizing/WhileOptimizer.cs
@@ -0,0 +1,26 @@
+﻿using LeoLang.Core;
+using LeoLang.Core.AST;
+using LeoLang.Core.AST.Statements;
+using System;
+
+namespace LeoLangCompiler.Strategies.Optimizing
+{
+    public class WhileOptimizer : IStrategy<SyntaxNode>
+    {
+        public SyntaxNode Do(SyntaxNode arg)
+        {
+            if (arg is WhileStatementNode node)
+            {
+                if (node.Condition is BooleanLiteralNode cond)
+                {
+                    if (!cond.Value)
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return arg;
+        }
+    }
+}
