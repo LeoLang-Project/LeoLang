acf2c5d3d510b9a6e23ddde00ec68a750f5cd434
Furesoft <filmee24@gmail.com>
2019-10-29 20:57:08 +0100

    grammar modifier fixed

--- LeoLangTests/ParserTests.cs
+++ LeoLangTests/ParserTests.cs
@@ -8,6 +8,12 @@ namespace Tests
 {
     public class ParserTests
     {
+        [Test]
+        public void CompilationUnit_Should_Pass()
+        {
+            var res = p.ParseCompilationUnit("void hello(void) { var x = false; }; void world(void) { var y = true; };");
+        }
+
         [SetUp]
         public void Setup()
         {
