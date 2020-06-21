1cdd5005ea9986b680b3d00c09c67aa7d3f9486d
Chris Anders <filmee24@gmail.com>
2020-06-21 20:56:56 +0200

    initialized new project

---
+++ LLC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LLC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while(true) {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(line)) {
+                    return;
+                }
+
+            }
+        }
+    }
+}
