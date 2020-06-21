0c0daf2b27dce8a2658804948d442b520f9ca28c
Chris Anders <filmee24@gmail.com>
2020-06-21 21:17:50 +0200

    added simple lexer

--- LLC/Program.cs
+++ LLC/Program.cs
@@ -10,7 +10,26 @@ namespace LLC
             while(true) {
                 Console.Write("> ");
                 var line = Console.ReadLine();
-                if(string.IsNullOrWhiteSpace(line)) {
+                var lexer = new Lexer(line);
+                while(true)
+                {
+                    var token = lexer.NextToken();
+                    if(token.Kind == SyntaxKind.EndOfFileToken)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write($"{token.Kind}: '{token.Text}'");
+                        if(token.Value != null)
+                        {
+                            Console.Write($" {token.Value}");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+                if (string.IsNullOrWhiteSpace(line)) {
                     return;
                 }
 
---
+++ LLC/SyntaxKind.cs
@@ -0,0 +1,16 @@
+﻿namespace LLC
+{
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        CloseParenthiseToken,
+        SlashToken,
+        OpenParenthiseToken,
+        StarToken,
+        BadToken,
+        EndOfFileToken
+    }
+}
