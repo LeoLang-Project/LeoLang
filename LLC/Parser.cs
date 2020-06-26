ca3f491b03d5bb80c364b368cc132158091cb726
Chris Anders <filmee24@gmail.com>
2020-06-26 15:36:09 +0200

    Fix Parsing Bug

--- LLC/Parser.cs
+++ LLC/Parser.cs
@@ -8,40 +8,44 @@ namespace LLC
     class Parser
     {
         private readonly SyntaxToken[] _tokens;
+
+        private List<string> _diagnostics = new List<string>();
         private int _position;
-        public List<string> Diagnostics = new List<string>();
 
         public Parser(string text)
         {
             var tokens = new List<SyntaxToken>();
+
             var lexer = new Lexer(text);
             SyntaxToken token;
             do
             {
                 token = lexer.NextToken();
 
-                if (token.Kind != SyntaxKind.WhitespaceToken && token.Kind != SyntaxKind.BadToken)
+                if (token.Kind != SyntaxKind.WhitespaceToken &&
+                    token.Kind != SyntaxKind.BadToken)
                 {
                     tokens.Add(token);
                 }
             } while (token.Kind != SyntaxKind.EndOfFileToken);
 
             _tokens = tokens.ToArray();
-            Diagnostics.AddRange(lexer.Diagnostics);
+            _diagnostics.AddRange(lexer.Diagnostics);
         }
+
+        public IEnumerable<string> Diagnostics => _diagnostics;
 
         private SyntaxToken Peek(int offset)
         {
             var index = _position + offset;
             if (index >= _tokens.Length)
-            {
-                return _tokens.Last();
-            }
+                return _tokens[_tokens.Length - 1];
 
             return _tokens[index];
         }
 
         private SyntaxToken Current => Peek(0);
+
         private SyntaxToken NextToken()
         {
             var current = Current;
@@ -51,29 +55,34 @@ namespace LLC
 
         private SyntaxToken Match(SyntaxKind kind)
         {
-            if (Current.Kind == kind) return NextToken();
+            if (Current.Kind == kind)
+                return NextToken();
 
-            Diagnostics.Add($"Error: unexpected Token <{Current.Kind}> expected <{kind}>");
-            return new SyntaxToken(kind, Current.Position, "", null);
+            _diagnostics.Add($"ERROR: Unexpected token <{Current.Kind}>, expected <{kind}>");
+            return new SyntaxToken(kind, Current.Position, null, null);
+        }
+
+        private ExpressionSyntax ParseExpression()
+        {
+            return ParseTerm();
         }
 
         public SyntaxTree Parse()
         {
-            var expression = ParseTerm();
-            var eofToken = Match(SyntaxKind.EndOfFileToken);
-
-            return new SyntaxTree(Diagnostics, expression, eofToken);
+            var expresion = ParseTerm();
+            var endOfFileToken = Match(SyntaxKind.EndOfFileToken);
+            return new SyntaxTree(_diagnostics, expresion, endOfFileToken);
         }
 
         private ExpressionSyntax ParseTerm()
         {
-            var left = ParsePrimaryExpression();
+            var left = ParseFactor();
 
-            while (Current.Kind == SyntaxKind.PlusToken || Current.Kind == SyntaxKind.MinusToken)
+            while (Current.Kind == SyntaxKind.PlusToken ||
+                   Current.Kind == SyntaxKind.MinusToken)
             {
                 var operatorToken = NextToken();
                 var right = ParseFactor();
-
                 left = new BinaryExpressionSyntax(left, operatorToken, right);
             }
 
@@ -84,31 +93,25 @@ namespace LLC
         {
             var left = ParsePrimaryExpression();
 
-            while (Current.Kind == SyntaxKind.StarToken || Current.Kind == SyntaxKind.SlashToken)
+            while (Current.Kind == SyntaxKind.StarToken ||
+                   Current.Kind == SyntaxKind.SlashToken)
             {
                 var operatorToken = NextToken();
                 var right = ParsePrimaryExpression();
-
                 left = new BinaryExpressionSyntax(left, operatorToken, right);
             }
 
             return left;
         }
 
-        private ExpressionSyntax ParseExpression()
-        {
-            return ParseTerm();
-        }
-
         private ExpressionSyntax ParsePrimaryExpression()
         {
-            if(Current.Kind == SyntaxKind.OpenParenthiseToken)
+            if (Current.Kind == SyntaxKind.OpenParenthiseToken)
             {
                 var left = NextToken();
-                var expr = ParseExpression();
+                var expression = ParseExpression();
                 var right = Match(SyntaxKind.CloseParenthiseToken);
-
-                return new ParenthesizedExpressionSyntax(left, expr, right);
+                return new ParenthesizedExpressionSyntax(left, expression, right);
             }
 
             var numberToken = Match(SyntaxKind.NumberToken);
