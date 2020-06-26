525f92c00fbc322bf14915ba592367000434aa39
Chris Anders <filmee24@gmail.com>
2020-06-26 16:30:54 +0200

    cleanup

--- LLC/CodeAnalysis/LiteralExpressionSyntax.cs
+++ LLC/CodeAnalysis/LiteralExpressionSyntax.cs
@@ -9,7 +9,7 @@ namespace Leo.CodeAnalysis
             LiteralToken = literalToken;
         }
 
-        public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+        public override SyntaxKind Kind => SyntaxKind.LiteralExpression;
 
         public SyntaxToken LiteralToken { get; }
 
