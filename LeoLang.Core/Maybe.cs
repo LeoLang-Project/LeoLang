d7b7c991e337db8f120a082afe0526499a08d2ec
Chris Anders <filmee24@gmail.com>
2020-06-27 12:19:34 +0200

    added maybe type to core lib

---
+++ LeoLang.Core/Maybe.cs
@@ -0,0 +1,23 @@
+﻿namespace LeoLang.Core
+{
+    /// <summary>
+    /// Provides a set of functions for creating optional values.
+    /// </summary>
+    public static class Maybe
+    {
+        /// <summary>
+        /// Wraps an existing value in an Option&lt;T&gt; instance.
+        /// </summary>
+        /// <param name="value">The value to be wrapped.</param>
+        /// <returns>An optional containing the specified value.</returns>
+        public static Maybe<T> Some<T>(T value) => new Maybe<T>(value, true);
+
+
+        /// <summary>
+        /// Creates an empty Option&lt;T&gt; instance.
+        /// </summary>
+        /// <returns>An empty optional.</returns>
+        public static Maybe<T> None<T>() => new Maybe<T>(default(T), false);
+
+    }
+}
