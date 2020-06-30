f3e50b604047b6a725f839892000074a23151f2f
Chris Anders <filmee24@gmail.com>
2020-06-30 17:33:41 +0200

    added default table

---
+++ LeoLang.CodeAnalysis/DefaultTable.cs
@@ -0,0 +1,29 @@
+﻿using LeoLang.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeoLang.CodeAnalysis
+{
+    internal static class DefaultTable
+    {
+        static DefaultTable()
+        {
+            AddValue("i32", 0);
+            AddValue("empty", Maybe.None<object>());
+            AddValue("date", DateTime.MinValue);
+        }
+
+        public static void AddValue(string type, object value)
+        {
+            _values.Add(type, value);
+        }
+
+        public static object GetValue(string type)
+        {
+            return _values[type];
+        }
+
+        private static Dictionary<string, object> _values = new Dictionary<string, object>();
+    }
+}
