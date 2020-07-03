7efa63012b9f97100a1b063d2a39e9b2dcad6081
Chris Anders <filmee24@gmail.com>
2020-07-03 17:23:51 +0200

    added syntaxnode span

--- LeoLang.CodeAnalysis/Syntax/TextSpan.cs
+++ LeoLang.CodeAnalysis/Syntax/TextSpan.cs
@@ -12,5 +12,11 @@ namespace LeoLang.CodeAnalysis.Syntax
         public int Start { get; }
         public int Length { get; }
         public int End => Start + Length;
+
+        public static TextSpan FromBounds(int start, int end)
+        {
+            var length = end - start;
+            return new TextSpan(start, length);
+        }
     }
 }
