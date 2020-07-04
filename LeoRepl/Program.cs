7f993b6c51262c880d1d60c3e3ab1ee3f54524f3
Chris Anders <filmee24@gmail.com>
2020-07-04 14:20:43 +0200

    added more powerful repl

---
+++ LeoRepl/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace LeoRepl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repl = new LeoRepl();
+            repl.Run();
+        }
+    }
+}
