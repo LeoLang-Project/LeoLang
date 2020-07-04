680a7701bfabfcd18c1427a6bd4459ff042c9e1e
Chris Anders <filmee24@gmail.com>
2020-07-04 14:46:30 +0200

    added bounderrorexpression

---
+++ LeoLang.CodeAnalysis/Binding/BoundErrorExpression.cs
@@ -0,0 +1,10 @@
+﻿using LeoLang.CodeAnalysis.Symbols;
+
+namespace LeoLang.CodeAnalysis.Binding
+{
+    internal sealed class BoundErrorExpression : BoundExpression
+    {
+        public override BoundNodeKind Kind => BoundNodeKind.ErrorExpression;
+        public override TypeSymbol Type => TypeSymbol.Error;
+    }
+}
