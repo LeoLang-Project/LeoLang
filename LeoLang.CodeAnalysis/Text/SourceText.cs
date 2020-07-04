942ed727fc8ae1ad1c93103483f4c3ad54e577cf
Chris Anders <filmee24@gmail.com>
2020-07-04 09:08:41 +0200

    cleanup

--- LeoLang.CodeAnalysis/Text/SourceText.cs
+++ LeoLang.CodeAnalysis/Text/SourceText.cs
@@ -15,6 +15,63 @@ namespace LeoLang.CodeAnalysis.Text
             Lines = ParseLines(this, text);
         }
 
+        public static SourceText From(string text)
+        {
+            return new SourceText(text);
+        }
+
+        private static ImmutableArray<TextLine> ParseLines(SourceText sourceText, string text)
+        {
+            var result = ImmutableArray.CreateBuilder<TextLine>();
+
+            var position = 0;
+            var lineStart = 0;
+
+            while (position < text.Length)
+            {
+                var lineBreakWidth = GetLineBreakWidth(text, position);
+
+                if (lineBreakWidth == 0)
+                {
+                    position++;
+                }
+                else
+                {
+                    AddLine(result, sourceText, position, lineStart, lineBreakWidth);
+
+                    position += lineBreakWidth;
+                    lineStart = position;
+                }
+            }
+
+            if (position >= lineStart)
+                AddLine(result, sourceText, position, lineStart, 0);
+
+            return result.ToImmutable();
+        }
+
+        private static void AddLine(ImmutableArray<TextLine>.Builder result, SourceText sourceText, int position, int lineStart, int lineBreakWidth)
+        {
+            var lineLength = position - lineStart;
+            var lineLengthIncludingLineBreak = lineLength + lineBreakWidth;
+            var line = new TextLine(sourceText, lineStart, lineLength, lineLengthIncludingLineBreak);
+            result.Add(line);
+        }
+
+        private static int GetLineBreakWidth(string text, int position)
+        {
+            var c = text[position];
+            var l = position + 1 >= text.Length ? '\0' : text[position + 1];
+
+            if (c == '\r' && l == '\n')
+                return 2;
+
+            if (c == '\r' || c == '\n')
+                return 1;
+
+            return 0;
+        }
+
         public ImmutableArray<TextLine> Lines { get; }
 
         public char this[int index] => _text[index];
@@ -45,63 +102,6 @@ namespace LeoLang.CodeAnalysis.Text
             }
 
             return lower - 1;
-        }
-
-        private static ImmutableArray<TextLine> ParseLines(SourceText sourceText, string text)
-        {
-            var result = ImmutableArray.CreateBuilder<TextLine>();
-
-            var position = 0;
-            var lineStart = 0;
-
-            while (position < text.Length)
-            {
-                var lineBreakWidth = GetLineBreakWidth(text, position);
-
-                if (lineBreakWidth == 0)
-                {
-                    position++;
-                }
-                else
-                {
-                    AddLine(result, sourceText, position, lineStart, lineBreakWidth);
-
-                    position += lineBreakWidth;
-                    lineStart = position;
-                }
-            }
-
-            if (position > lineStart)
-                AddLine(result, sourceText, position, lineStart, 0);
-
-            return result.ToImmutable();
-        }
-
-        private static void AddLine(ImmutableArray<TextLine>.Builder result, SourceText sourceText, int position, int lineStart, int lineBreakWidth)
-        {
-            var lineLength = position - lineStart;
-            var lineLengthIncludingLineBreak = lineLength + lineBreakWidth;
-            var line = new TextLine(sourceText, lineStart, lineLength, lineLengthIncludingLineBreak);
-            result.Add(line);
-        }
-
-        private static int GetLineBreakWidth(string text, int position)
-        {
-            var c = text[position];
-            var l = position + 1 >= text.Length ? '\0' : text[position + 1];
-
-            if (c == '\r' && l == '\n')
-                return 2;
-
-            if (c == '\r' || c == '\n')
-                return 1;
-
-            return 0;
-        }
-
-        public static SourceText From(string text)
-        {
-            return new SourceText(text);
         }
 
         public override string ToString() => _text;
