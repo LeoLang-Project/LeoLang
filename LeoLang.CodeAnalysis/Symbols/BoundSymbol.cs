547e0000dc4b53f89f3821cd1223231a6b188560
Chris Anders <filmee24@gmail.com>
2020-07-04 14:46:20 +0200

    added boundsymbol

---
+++ LeoLang.CodeAnalysis/Symbols/BoundSymbol.cs
@@ -0,0 +1,14 @@
+﻿namespace LeoLang.CodeAnalysis.Symbols
+{
+    internal sealed class BoundSymbol
+    {
+        internal BoundSymbol(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public override string ToString() => Name;
+    }
+}
