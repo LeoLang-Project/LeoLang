95eeef8fbcef4edd7d1eb2ff9f3db651482eb003
Chris Anders <filmee24@gmail.com>
2020-07-04 14:47:41 +0200

    added typesymbol

---
+++ LeoLang.CodeAnalysis/Symbols/TypeSymbol.cs
@@ -0,0 +1,21 @@
+﻿namespace LeoLang.CodeAnalysis.Symbols
+{
+    public sealed class TypeSymbol : Symbol
+    {
+        public static readonly TypeSymbol Bool = new TypeSymbol("bool");
+        public static readonly TypeSymbol Int = new TypeSymbol("int");
+        public static readonly TypeSymbol String = new TypeSymbol("string");
+        public static readonly TypeSymbol Symbol = new TypeSymbol("symbol");
+        public static readonly TypeSymbol Some = new TypeSymbol("some");
+        public static readonly TypeSymbol Any = new TypeSymbol("any");
+        public static readonly TypeSymbol Type = new TypeSymbol("type");
+        public static readonly TypeSymbol Error = new TypeSymbol("?");
+
+        private TypeSymbol(string name)
+            : base(name)
+        {
+        }
+
+        public override SymbolKind Kind => SymbolKind.Type;
+    }
+}
