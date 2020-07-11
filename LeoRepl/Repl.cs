7245cb1166b5f34e5978c1c9b8ff1547e6105354
Chris Anders <filmee24@gmail.com>
2020-07-11 15:46:17 +0200

    cleanup

--- LeoRepl/Repl.cs
+++ LeoRepl/Repl.cs
@@ -80,7 +80,7 @@ namespace LeoRepl
         {
             private readonly Action<string> _lineRenderer;
             private readonly ObservableCollection<string> _submissionDocument;
-            private readonly int _cursorTop;
+            private int _cursorTop;
             private int _renderedLineCount;
             private int _currentLine;
             private int _currentCharacter;
@@ -107,6 +107,14 @@ namespace LeoRepl
 
                 foreach (var line in _submissionDocument)
                 {
+                    if (_cursorTop + lineCount >= Console.WindowHeight)
+                    {
+                        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                        Console.WriteLine();
+                        if (_cursorTop > 0)
+                            _cursorTop--;
+                    }
+
                     Console.SetCursorPosition(0, _cursorTop + lineCount);
                     Console.ForegroundColor = ConsoleColor.Green;
 
