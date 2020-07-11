0fb010e14d1892c4bf4adb648fe476ea67d4aa85
Chris Anders <filmee24@gmail.com>
2020-07-11 15:40:24 +0200

    made metacommands attribute-driven

---
+++ LeoRepl/MetaCommandAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace LeoRepl
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class MetaCommandAttribute : Attribute
+    {
+        public MetaCommandAttribute(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+    }
+    public sealed class MetaCommand
+    {
+        public MetaCommand(string name, string description, MethodInfo method)
+        {
+            Name = name;
+            Description = description;
+            Method = method;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+        public MethodInfo Method { get; }
+    }
+}
